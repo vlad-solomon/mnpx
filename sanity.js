2cc33c953d53e4335a83712648ed5a6c4b783ac6
Vlad Solomon <vladsolomon720@gmail.com>
2024-10-12 13:33:28 +0300

    Prototype fetching from sanity

---
+++ sanity.js
@@ -0,0 +1,22 @@
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = createClient({
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+    dataset: import.meta.env.VITE_SANITY_DATASET,
+    useCdn: true,
+    apiVersion: import.meta.env.VITE_API_VERSION,
+});
+
+export async function getPhotos() {
+    const photos = await client.fetch(
+        "*[_type == 'photo' && archived == false]{location{lng,lat},image{asset{_ref}},slug{current},tags,date}"
+    );
+    return photos;
+}
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source) {
+    return builder.image(source);
+}
